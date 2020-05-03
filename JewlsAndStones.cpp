9afe835d49616d70954271d472ce5406f3b55cb5
iLoveCrusts <46233248+iLoveCrusts@users.noreply.github.com>
2020-05-02 23:58:37 -0400

    Update JewlsAndStones.cpp

--- JewlsAndStones.cpp
+++ JewlsAndStones.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int numJewelsInStones(string J, string S) {
         int output = 0;
+        //I brute forced by traversing every char in the J and then compare with every char in S
         for (std::string::iterator it1 = J.begin(); it1 != J.end(); ++it1 ){
             char i = *it1;
             for (std::string::iterator it2  = S.begin(); it2 != S.end(); ++it2){
@@ -9,8 +10,9 @@ public:
                     output++;
                 }
             }
-            i = NULL;
+            i = NULL;//reset the char to be null in order to traverse to next type in J
         }
         return output;
+        //the time limit passed but i expect it to be more efficient later
     }
 };
