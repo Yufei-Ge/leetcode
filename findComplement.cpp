3049679b5a0a9e80656bb0ccaa56f3072cb17be8
iLoveCrusts <46233248+iLoveCrusts@users.noreply.github.com>
2020-05-04 23:45:03 -0400

    Update findComplement.cpp

--- findComplement.cpp
+++ findComplement.cpp
@@ -1,5 +1,28 @@
-//
-// Created by feifei on 2020-05-04.
-//
-
-#include "findComplement.h"
+//question: Given an integer, find its binary complement and return the decimal value of that binary complement
+class Solution {
+public:
+    int findComplement(int num) {
+        int bin_complement=0;
+        std::string binary;
+        //convert decimal to binary and store in a string
+        while(num!=0) {binary=(num%2==0 ?"0":"1")+binary; num/=2;}
+        //flipping to get complement binary string
+        for (char& c : binary){
+           
+            if (c=='1'){
+                c='0';
+            }else if(c=='0') {c='1';}
+        }
+        //to get the decimal number of binary complement
+        int c = binary.size();
+        for (int i=0; i<c; i++){
+            if (binary[i]=='1'){
+                bin_complement += (int) pow (2, ((c-1)-i));
+            }
+        }
+    
+        return bin_complement;
+        
+        
+    }
+};
